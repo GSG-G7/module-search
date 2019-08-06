8819c1771318e6855f8e1f8d08a512dfa62a596f
asem1789 <as.1789@hotmail.com>
2019-08-06 16:03:03 +0300

    handle search route and make api call from back Relates #10

--- public/js/request.js
+++ public/js/request.js
@@ -3,8 +3,16 @@ const input = document.querySelector('.input');
 
 btn.addEventListener('click', (e) => {
   e.preventDefault();
-  console.log('hello', input.value)
-  fetch(`/search?query=${input.value}`)
+  const inputValue = input.value;
+  // console.log('hello', input.value)
+  fetch('/search', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ inputValue })
+  })
     .then(res => res.json())
+    .then(res => console.log(res.data))
     .catch(err => console.log(err));
+
+  input.value = '';
 })
