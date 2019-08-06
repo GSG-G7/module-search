41fc1069a51d9c96779940fa3b4923bcf0842455
asem1789 <as.1789@hotmail.com>
2019-08-06 14:56:11 +0300

    handling home and adding test for it Relates #6

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
@@ -6,5 +8,22 @@ const port = process.env.PORT || 5000;
 app.set('port', port);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  exphbs({
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    defaultLayout: 'main',
+  })
+)
+
+app.get('/', (req, res) => {
+  res.render("home")
+})
+
 
 module.exports = app;
