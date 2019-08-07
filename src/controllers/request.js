10bf8e782ca29aa7301434df72972f5eac762f0b
asem1789 <as.1789@hotmail.com>
2019-08-07 15:29:09 +0300

    hide api-key Relates #19

--- src/controllers/request.js
+++ src/controllers/request.js
@@ -1,9 +1,11 @@
 const nodefetch = require('node-fetch');
+require('dotenv/config');
 
 exports.getData = (req, res) => {
+  const apiKey = process.env.API_KEY;
   const npmModule = req.body.input;
   console.log(req.body.input, '*********')
-  const url = `https://libraries.io/api/NPM/${npmModule}?api_key=9d6daf8cc3a087418203a84deaf6164d`;
+  const url = `https://libraries.io/api/NPM/${npmModule}?api_key=${apiKey}`;
   nodefetch(url).then(res => res.json())
     .then(resp => {
       res.render("home", {
