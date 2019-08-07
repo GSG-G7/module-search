800eb93e8b38333a541cecaca9d6a3b901447bf0
hashemt <hashemta77@gmail.com>
2019-08-07 12:06:56 +0300

    render element Relates #11

--- src/controllers/request.js
+++ src/controllers/request.js
@@ -1,12 +1,12 @@
 const nodefetch = require('node-fetch');
 
 exports.getData = (req, res) => {
-  const npmModule = req.body.inputValue;
-  console.log(req.body)
+  const npmModule = req.body.input;
+  console.log(req.body.input,'*********')
   const url = `https://libraries.io/api/NPM/${npmModule}?api_key=9d6daf8cc3a087418203a84deaf6164d`;
   nodefetch(url).then(res => res.json())
     .then(resp => {
-      console.log(resp.description)
+      // console.log(resp.description)
       res.render("home", {
         result: resp.description
       })
