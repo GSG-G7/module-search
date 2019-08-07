ab1c7ab3b09ff94929b4b45f1c95ce883d0a08a9
asem1789 <as.1789@hotmail.com>
2019-08-07 11:11:05 +0300

    make render to data from server into client Relates #11

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ app.engine(
   exphbs({
     extname: 'hbs',
     layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
     defaultLayout: 'main',
   })
 )
